ce6ebf04966fd6fb84a4f069fa0ed501a1451a04
SplunkQueen <nila.y.curry@gmail.com>
2021-10-27 11:37:18 -0400

    made changes

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def print_hi(name):
+//Print a message
+
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
